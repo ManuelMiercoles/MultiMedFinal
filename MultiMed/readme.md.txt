ad34e0019a364875438f49caf504357cff58be0a
MannyTrejo <40349566+MannyTrejo@users.noreply.github.com>
2019-07-31 15:51:55 -0500

    Fixing file, saving index.php to index.html
    Fixing minor errors, writing readme.

---
+++ MultiMed/readme.md.txt
@@ -0,0 +1,10 @@
+Run mysql, mariadb, etc using XAMPP,WAMPP, or engine of your choice. 
+database.sql and config.php automatically generate a database if none exist, 
+provided mysql or other db is running.
+
+Database created in config.php
+Tables to fill db generated in database.sql
+Open index.html first. 
+
+Modify function has not been implemented, once one has logged-in
+as admin, they may enter new users or delete existing entries.
--- MultiMed/save.php
+++ MultiMed/save.php
@@ -45,34 +45,36 @@ mysqli_query($db,$query);
 					<div class="card-body">
 <form action="save.php" method="post">
 	<br />Register New Student<br />
-	<p>&nbsp;</p>
-	<p class="auto-style2">&nbsp;Name:
-	<input name="Name" style="width: 222px" type="text" /> <br /><br />&nbsp;User:
+	<p></p>
+	<p class="auto-style2"> Name:
+	<input name="Name" style="width: 222px" type="text" /> <br /><br /> User:
 	<input class="auto-style1" name="User" style="width: 217px" type="text" />
 	<br />
 	<br />Sexo:&nbsp; <select name="Sex" class="auto-style4">
 				<option value="1">Masculino</option>
 				<option value="2">Femenino</option>
-			</select>.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Edad:<input name="Age" type="text" maxlength="2" style="width: 30px;"/></p>
-	<p class="auto-style2">Carrera:&nbsp; <select name="Carrera" >
+			</select>Edad:<input name="Age" type="text" maxlength="2" style="width: 30px;"/></p>
+	<p class="auto-style2">Carrera:  <select name="Carrera" >
 	<option value="1">Ing. en Videojuegos</option>
 	<option value="2">Medios Interactivos</option>
 	<option value="3">Psicologia</option>
 	<option value="4">Filosofia</option>
 	<option value="5">Biologia</option>
 	<option value="6">Medicina</option>
-	</select>&nbsp;&nbsp;</p>
-	<p class="auto-style2">&nbsp;&nbsp; Horas Totales:
+	</select></p>
+	<p class="auto-style2"> Horas Totales:
 	<input name="Hours" type="text" style="width: 60px;" /><br />
 	<br />
-	Dias de Clase&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	Dias de Clase
+
 	<br />
 	Lunes: <input name="Days" type="checkbox" value="Lunes" /> <br />
-	Martes:<input name="Days" type="checkbox" value ="Martes"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	Martes:<input name="Days" type="checkbox" value ="Martes"/>&nbsp;&nbsp;
 	<br />
 	Miercoles:<input name="Days" type="checkbox" value="Miercoles" /> <br />
 	Jueves:<input name="Days" type="checkbox"  value="Jueves"/> <br />
-	Viernes:<input name="Days" type="checkbox" value="Viernes" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	Viernes:<input name="Days" type="checkbox" value="Viernes" />
+
 </br><input name="Ingresa" style="width: 142px" class="btn btn-primary" type="submit" value="Ingresa" /></p>
 	</form>
 	</div>
